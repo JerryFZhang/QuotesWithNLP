199f384a379f4e710434ee781d4ccc89d85cc2c0
Jerry F. Zhang <jerry.fengwei@gmail.com>
2020-11-14 16:54:37 -0500

    imported data as a module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import * as data from "../../db/quotes.json";
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(data);
 })
 
 app.listen(port, () => {
+  // tslint:disable-next-line: no-console
   console.log(`Example app listening at http://localhost:${port}`)
 })
